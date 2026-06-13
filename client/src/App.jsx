3ad2654f9a55d264b852d6a66bd9e6e71e8d362b
ParekhVaibhavi11 <vaibhaviparekh2812@gmail.com>
2026-06-13 20:46:42 +0530

    Initial MERN project setup with React Vite and Express

---
+++ client/src/App.jsx
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <div className="min-h-screen flex items-center justify-center bg-purple-100">
+      <h1 className="text-5xl font-bold text-purple-700">
+        Welcome to Padhlo 🚀
+      </h1>
+    </div>
+  );
+}
+
+export default App;
