dd0fa9fe61d88e11cd1215ad12f13f07f417280b
ParekhVaibhavi11 <vaibhaviparekh2812@gmail.com>
2026-06-15 09:41:24 +0530

    solve import issue

--- server/routes/ClassroomRoutes.js
+++ server/routes/ClassroomRoutes.js
@@ -14,6 +14,7 @@ const {
   "../controllers/classroomController"
 );
 
+
 const {
   protect,
 } = require(
--- server/server.js
+++ server/server.js
@@ -26,7 +26,7 @@ app.use(
 app.use(
   "/api/classrooms",
   require(
-    "./routes/classroomRoutes"
+    "./routes/ClassroomRoutes"
   )
 );
 
